63c305b02df88520dd8af3794d80b00b02051768
BharathaSachintha <sachinthabharatha123@gmail.com>
2021-08-03 13:15:33 +0530

    release

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify,request
+import time
+
+import flask
+
+app=flask(__name__)
+@app.route("/bot",method=["POST"])
+
+def response():
+    query=dict(request.form)['query']
+    result=query+" "+time.ctime()
+    return jsonify({"response":result})
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",)
